93736735f14f371d1a795581efdecea53274200b
Sowmiyasaravanan-13 <sowmiyara96@gmail.com>
2024-04-02 21:09:24 +0530

    Update Project

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+function Navbar()
+{
+   return(
+    <div>
+       <nav>
+        <h1>Perfumy</h1>
+        <ul>
+          <li>Home |</li>
+          <li>Products |</li>
+          <li><a href="#">About |</a></li>
+          <li><a href="#">Contact |</a></li>
+
+        </ul>
+       </nav>
+    </div>
+   )
+}
+
+export default Navbar
---
+++ src/components/Search.js
@@ -0,0 +1,7 @@
+function Search()
+{
+  return(<div className="searchbar">
+       <input type="text" placeholder="Search"></input>
+  </div>)
+}
+export default Search
